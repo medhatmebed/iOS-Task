90bb86c189ad5c55440c527656a4aadd08e0d43d
Medhat Mebed <medhat.mebed@gmail.com>
2019-02-16 19:32:14 +0200

    setup Project iOS Task

---
+++ iOS Task/Controller/ProductDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProductDetailVC.swift
+//  iOS Task
+//
+//  Created by Medhat Mebed on 2/16/19.
+//  Copyright © 2019 Medhat Mebed. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
